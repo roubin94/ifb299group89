2983c04644aba941f5badb55c6d3c8a23c46eaf5
Daniel Wan <daniel.wan@live.com.au>
2016-09-07 18:47:54 +1000

    Added Jason stuff

--- Template.php
+++ Template.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?php echo $title; ?></title>
-        <link rel="stylesheet" type="text/css" href="./Styles/Stylesheet.css" />
+        <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
     </head>
     <body>
         <div id="wrapper">
