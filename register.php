f9b382552864448c9a5ce2a14b6bf5ae4792a3ba
Daniel Wan <daniel.wan@live.com.au>
2016-09-07 22:24:11 +1000

    Worked on register page

--- register.php
+++ register.php
@@ -16,51 +16,79 @@ $emailErr = $passwordErr = $first_nameErr = $last_nameErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Check if inputted e-mail adress is valid.
-  $email = test_input($_POST["email"]);
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $emailErr = "Invalid email format"; 
+  if (empty($_POST["email"])) {
+    $emailErr = "Your e-mail address is required.";
   }
-  // Check if inputted password is valid.
-  $password = htmlspecialchars($_POST["password"]);
-  $password_repeat = test_input($_POST["password_repeat"]);
-  if ($password != $password_repeat) {
-    $passwordErr = "Your entered passwords did not match.";
+  else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
   }
+  // Check if inputted passwords are valid.
+  if (empty($_POST["password"])) {
+    $passwordErr = "Please enter a password.";
+  }
+  else if (empty($_POST["password_repeat"])){
+    $password_repeatErr = "Please repeat your password.";
+  }
+  else {
+    $password = test_input($_POST["password"]);
+    $password_repeat = test_input($_POST["password_repeat"]);
+    if ($password != $password_repeat) {
+      $passwordErr = "Your entered passwords did not match.";
+    }
+  }
+  
   // Check if inputted first name is valid.
-  $first_name = test_input($_POST["first_name"]);
-  if (!preg_match("/^[a-zA-Z-']*$/",$name)) {
-    $first_nameErr = "Your first name contains invalid characters for our system."; 
+  if (empty($_POST["first_name"])) {
+    $first_nameErr = "Your first name is required.";
   }
+  else {
+    $first_name = test_input($_POST["first_name"]);
+    if (!preg_match("/^[a-zA-Z-']*$/",$name)) {
+      $first_nameErr = "Your first name contains invalid characters for our system."; 
+    }
+  }
+  
   // Check if inputter last name is valid.
-  $last_name = test_input($_POST["last_name"]);
-  if (!preg_match("/^[a-zA-Z-']*$/",$name)) {
-    $last_nameErr = "Your last name contains invalid characters for our system."; 
+  if (empty($_POST["last_name"])) {
+    $first_nameErr = "Your last name is required.";
+  }
+  else {
+    $last_name = test_input($_POST["last_name"]);
+    if (!preg_match("/^[a-zA-Z-']*$/",$name)) {
+      $last_nameErr = "Your last name contains invalid characters for our system."; 
+    }
   }
 }
 
+// Function to avoid unwanted inputs.
 function test_input($data) {
   $data = trim($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
 ?>
 
 <h2>Registration Page</h2>
 <a href="index.php">Click here to go back.</a><br /><br />
-        
+
+<!--Registration form-->   
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-  E-mail Address: <input type="text" name="email" required="Your e-mail address is required."/>
+  E-mail Address: <input type="text" name="email" value="<?php echo $email; ?>">
     <span class="error"><?php echo $emailErr; ?></span>
     <br />
-    Password: <input type="password" name="password" required="A password is required."/>
+    Password: <input type="password" name="password">
     <span class="error"><?php echo $passwordErr; ?></span>
     <br />
-    Repeat Password: <input type="password" name="password_repeat" required="Please re-enter your password."/>
+    Repeat Password: <input type="password" name="password_repeat">
     <br />   
-    First Name: <input type="text" name="first_name" required="Your first name is required."/>
+    First Name: <input type="text" name="first_name" value="<?php echo $first_name; ?>">
     <span class="error"><?php echo $emailErr; ?></span>
     <br />
-    Last Name: <input type="text" name="last_name" required="Your last name is required."/>
+    Last Name: <input type="text" name="last_name" value="<?php echo $last_name; ?>">
     <span class="error"><?php echo $emailErr; ?></span>
     <br />
     <input type="submit" value="Register"/>
