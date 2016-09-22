eba7402d89509886b3886486e6528d2b6813b15d
roubin94 <roubin.94@hotmail.com>
2016-09-22 14:26:12 +1000

    unnecessary line of code

--- book_slots.php
+++ book_slots.php
@@ -2,7 +2,6 @@
 
 include('db_connect_bookings.php'); 
 include "header.php"; 
-session_start();
 
 if(isset($_POST['slots_booked'])) $slots_booked = mysqli_real_escape_string($link, $_POST['slots_booked']);
 if(isset($_POST['name'])) $name = mysqli_real_escape_string($link, $_POST['name']);
