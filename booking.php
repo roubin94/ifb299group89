c4f6eae911e0f48575cc6eb292781c2415021e85
roubin94 <roubin.94@hotmail.com>
2016-09-22 14:06:35 +1000

    update SQL connection file

--- booking.php
+++ booking.php
@@ -5,7 +5,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-include('php/connect.php'); 
+include('db_connect_bookings.php'); 
 include('classes/class_calendar.php');
 
 $calendar = new booking_diary($link);
